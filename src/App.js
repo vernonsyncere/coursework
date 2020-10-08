c09a7d6f3289a61382ca4ef11e17bf8f6c3c5cfa
Vernon Lewis <leesyncere@gmail.com>
2020-10-08 11:58:46 -0700

    added component, passed props, and children

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hi Im a react App</h1>
+        <p>This is working</p>
+        <Person name="max" age="28"/>
+        <Person name="manu" age="25">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="35"/>
       </div>
     );
   }
