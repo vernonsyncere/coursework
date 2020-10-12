4810ac24e260cceb3f9f2035ff9356b7ff2fa185
Vernon Lewis <leesyncere@gmail.com>
2020-10-12 14:49:25 -0700

    used Radium and StyleRoot form radium to used advanced css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+
 import React, { Component } from 'react';
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
 import './App.css';
+import Radium, { StyleRoot } from  'radium';
 import Person from './Person/Person';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
@@ -59,11 +62,16 @@ class App extends Component {
   }
   render() {
     const style ={
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color:'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor:'lightgreen',
+        color:'black'
+      }
     };
 
     const newStyle ={
@@ -89,29 +97,48 @@ class App extends Component {
             changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
 
+          
+
           {/* <UserOutput newStyle={newStyle} username={this.state.usernames[0].username}/>
           <UserInput newStyle={newStyle} typed={this.usernameChangedHandler.bind(this)} username={this.state.usernames[0].username}/>
 
           <UserOutput newStyle={newStyle} username={this.state.usernames[1].username}/> */}
         </div> 
+       
       )
+      style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor:'salmon',
+        color:'black'
+      };
+
+      
     }
   
-    
+    const classes = [];
+    if (this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1){
+      classes.push('bold');
+    }
     return (
-      <div className="App">
-        <h1> Hi Im a react App</h1>
-        <p>This is working</p>
-        <button 
-        style={style}
-        onClick={this.togglePersonHandler}>Switch Name</button>
-        {/* {this.state.showPersons === true ? */}
-         {persons}
-        {/* : null } */}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1> Hi Im a react App</h1>
+          <p className={classes.join(' ')}>This is working</p>
+          <button 
+          style={style}
+          onClick={this.togglePersonHandler}>Switch Name</button>
+          {/* {this.state.showPersons === true ? */}
+          {persons}
+          {/* : null } */}
+        </div>
+      </StyleRoot>
+      
     );
   }
 
 }
 
-export default App;
+export default Radium(App);
