a6549298a15d11dc6e8e420ec4e54c2d4c2eeff9
Vernon Lewis <leesyncere@gmail.com>
2020-10-12 20:03:19 -0700

    changed to css Module

--- src/App.js
+++ src/App.js
@@ -1,144 +1,147 @@
+import React, { Component } from "react";
+import { findRenderedComponentWithType } from "react-dom/test-utils";
+import classes from "./App.module.css";
+import Person from "./Person/Person";
+import UserInput from "./UserInput/UserInput";
+import UserOutput from "./UserOutput/UserOutput";
 
-import React, { Component } from 'react';
-import { findRenderedComponentWithType } from 'react-dom/test-utils';
-import './App.css';
-import Radium, { StyleRoot } from  'radium';
-import Person from './Person/Person';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
+// const StyledButton = styled.button`
+//   background-color: ${props => props.alt ? 'red' : 'green'};
+//   color: white;
+//   font: inherit;
+//   border: 1px solid blue;
+//   padding: 8px;
+//   cursor: pointer;
 
+//   &:hover {
+//     background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+//     color: black;
+//   }
+// `;
 class App extends Component {
-  state = {
-    persons: [
-      { id:"1", name: 'max', age: 28 },
-      { id:"2", name: 'manu', age: 25 },
-      { id:"3", name: 'stephanie', age: 35 }
-    ],
-    otherState : 'other value',
-    usernames: [
-      {username: "Syncere"},
-      {username: "LikeyMe"}
-    ], 
-    showPersons: false
-  }
 
   
+  state = {
+    persons: [
+      { id: "1", name: "max", age: 28 },
+      { id: "2", name: "manu", age: 25 },
+      { id: "3", name: "stephanie", age: 35 },
+    ],
+    otherState: "other value",
+    usernames: [{ username: "Syncere" }, { username: "LikeyMe" }],
+    showPersons: false,
+  };
+
   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
+    const personIndex = this.state.persons.findIndex((p) => {
       return p.id === id;
     });
 
     const person = {
-      ...this.state.persons[personIndex]
+      ...this.state.persons[personIndex],
     };
 
-    person.name = event.target.value
+    person.name = event.target.value;
 
     const persons = [...this.state.persons];
-    
-    persons[personIndex] = person
-    
-    this.setState( { persons: persons} )
-  }
-  usernameChangedHandler = (event) =>{
-    this.setState( { 
-      usernames: [
-        {username: event.target.value},
-        {username: "Syncere"}
-      ] 
-    } )
-  }
 
-  deletePersonhandler = (personIndex)=>{
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  };
+  usernameChangedHandler = (event) => {
+    this.setState({
+      usernames: [{ username: event.target.value }, { username: "Syncere" }],
+    });
+  };
+
+  deletePersonhandler = (personIndex) => {
     // const persons = this.state.persons.slice();
-    const persons =[...this.state.persons]
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons})
-  }
+    this.setState({ persons: persons });
+  };
 
   togglePersonHandler = () => {
-   const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
-  }
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
   render() {
-    const style ={
-      backgroundColor: 'green',
-      color:'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor:'lightgreen',
-        color:'black'
+    const style = {
+      backgroundColor: "green",
+      color: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black",
       }
     };
 
-    const newStyle ={
-      backgroundColor: 'black',
-      color:'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+    const newStyle = {
+      backgroundColor: "black",
+      color: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
     };
 
     let persons = null;
+    let btnClass = [classes.Button]
 
-    if( this.state.showPersons) {
+    if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
-            click={() => this.deletePersonhandler(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)} />
+            return (
+              <Person
+                click={() => this.deletePersonhandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            );
           })}
-
-          
 
           {/* <UserOutput newStyle={newStyle} username={this.state.usernames[0].username}/>
           <UserInput newStyle={newStyle} typed={this.usernameChangedHandler.bind(this)} username={this.state.usernames[0].username}/>
 
           <UserOutput newStyle={newStyle} username={this.state.usernames[1].username}/> */}
-        </div> 
-       
-      )
-      style.backgroundColor = 'red'
-      style[':hover'] = {
-        backgroundColor:'salmon',
-        color:'black'
-      };
+        </div>
+      );
 
-      
+      btnClass.push(classes.Red)
+      // style.backgroundColor = "red";
+      // style[":hover"] = {
+      //   backgroundColor: "salmon",
+      //   color: "black",
+      // };
     }
-  
-    const classes = [];
-    if (this.state.persons.length <= 2){
-      classes.push('red');
+
+    const assignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      assignedClasses.push(classes.red);
     }
-    if (this.state.persons.length <= 1){
-      classes.push('bold');
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold);
     }
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1> Hi Im a react App</h1>
-          <p className={classes.join(' ')}>This is working</p>
-          <button 
-          style={style}
-          onClick={this.togglePersonHandler}>Switch Name</button>
-          {/* {this.state.showPersons === true ? */}
-          {persons}
-          {/* : null } */}
-        </div>
-      </StyleRoot>
-      
+      <div className={classes.App}>
+        <h1> Hi Im a react App</h1>
+        <p className={assignedClasses.join(" ")}>This is working</p>
+        <button className={btnClass.join(' ')} alt={this.state.showPersons} onClick={this.togglePersonHandler}>
+          Switch Name
+        </button>
+        {/* {this.state.showPersons === true ? */}
+        {persons}
+        {/* : null } */}
+      </div>
     );
   }
-
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,36 @@
 import React from 'react'
-import './Person.css';
-import Radium from 'radium'
+import classes from './Person.module.css';
+import styled from 'styled-components'
 
 const Person = (props) => {
+const StyledDiv =  styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding:16px;
+
+
+    @media (min-width: 500px) {
+            width: 450px;
+    }
+`
+
     const style = {
         '@media (min-width: 500px)' : {
             width: '450px'
         }
     }
     return (
-        <div className="Person" style={style}>
+        <div className={classes.Person} style={style}>
+       <button >
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+       </button>
+       </div>
+  
     )
 }
 
-export default Radium(Person);
+export default Person;
