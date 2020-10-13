6b4450a6454dc4bfb276985a16a5a6b232e69df1
Vernon Lewis <leesyncere@gmail.com>
2020-10-13 11:10:11 -0700

    cleaned code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React, { Component } from "react";
-import { findRenderedComponentWithType } from "react-dom/test-utils";
+// Importing from a css module
 import classes from "./App.module.css";
 import Person from "./Person/Person";
-import UserInput from "./UserInput/UserInput";
-import UserOutput from "./UserOutput/UserOutput";
+// import UserInput from "./UserInput/UserInput";
+// import UserOutput from "./UserOutput/UserOutput";
 
-// const StyledButton = styled.button`
-//   background-color: ${props => props.alt ? 'red' : 'green'};
-//   color: white;
-//   font: inherit;
-//   border: 1px solid blue;
-//   padding: 8px;
-//   cursor: pointer;
-
-//   &:hover {
-//     background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-//     color: black;
-//   }
-// `;
 class App extends Component {
-
-  
   state = {
     persons: [
       { id: "1", name: "max", age: 28 },
@@ -33,65 +18,56 @@ class App extends Component {
   };
 
   nameChangedHandler = (event, id) => {
+    // assigning an index to the variable personIndex
+    // by returning the index of the changed array
     const personIndex = this.state.persons.findIndex((p) => {
       return p.id === id;
     });
 
+    // assign the person at the index to the variable person
     const person = {
       ...this.state.persons[personIndex],
     };
 
+    // change the person to whatever is being typed in
     person.name = event.target.value;
 
+    // making a copy of state the way it currently is
     const persons = [...this.state.persons];
 
+    // updated persons at the index and the value that was typed in
     persons[personIndex] = person;
 
+    // set state to the new updated state with the modifcation to whichever field
     this.setState({ persons: persons });
   };
   usernameChangedHandler = (event) => {
+    // changing the username of the first two indexs in state. the first by what ever is typed in
+    // the second is hard coded to "syncere"
     this.setState({
       usernames: [{ username: event.target.value }, { username: "Syncere" }],
     });
   };
 
   deletePersonhandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
+    // assigning the current state to the variable persons
     const persons = [...this.state.persons];
+    // removing one person at the index provided
     persons.splice(personIndex, 1);
+    // seting persons in state to what the varibale persons above is after it is spliced
     this.setState({ persons: persons });
   };
 
   togglePersonHandler = () => {
+    // assigning the value of showPersons in state to the variable doesShow
     const doesShow = this.state.showPersons;
+    // sets state at showPersons to be the opposite of what doesShow is
     this.setState({ showPersons: !doesShow });
   };
   render() {
-    const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-      ":hover": {
-        backgroundColor: "lightgreen",
-        color: "black",
-      }
-    };
-
-    const newStyle = {
-      backgroundColor: "black",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-    };
-
     let persons = null;
-    let btnClass = [classes.Button]
-
+    let btnClass = [classes.Button];
+    console.log("me", btnClass);
     if (this.state.showPersons) {
       persons = (
         <div>
@@ -106,20 +82,10 @@ class App extends Component {
               />
             );
           })}
-
-          {/* <UserOutput newStyle={newStyle} username={this.state.usernames[0].username}/>
-          <UserInput newStyle={newStyle} typed={this.usernameChangedHandler.bind(this)} username={this.state.usernames[0].username}/>
-
-          <UserOutput newStyle={newStyle} username={this.state.usernames[1].username}/> */}
         </div>
       );
 
-      btnClass.push(classes.Red)
-      // style.backgroundColor = "red";
-      // style[":hover"] = {
-      //   backgroundColor: "salmon",
-      //   color: "black",
-      // };
+      btnClass.push(classes.Red);
     }
 
     const assignedClasses = [];
@@ -133,7 +99,11 @@ class App extends Component {
       <div className={classes.App}>
         <h1> Hi Im a react App</h1>
         <p className={assignedClasses.join(" ")}>This is working</p>
-        <button className={btnClass.join(' ')} alt={this.state.showPersons} onClick={this.togglePersonHandler}>
+        <button
+          className={btnClass.join(" ")}
+          alt={this.state.showPersons}
+          onClick={this.togglePersonHandler}
+        >
           Switch Name
         </button>
         {/* {this.state.showPersons === true ? */}
