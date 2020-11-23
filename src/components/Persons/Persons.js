c40b27eff6a6c9de97595657945f9a2afd3fd648
Vernon Lewis <leesyncere@gmail.com>
2020-11-23 11:52:50 -0800

    pure component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, {Component} from "react";
+import React, {PureComponent} from "react";
 import Person from "./Person/Person";
 
-class Persons extends Component {
+class Persons extends PureComponent {
   // static getDerivedStateFromProps(props, state){
   //   console.log('[Persons.js] getDerivedStateFromProps')
   //   return state;
@@ -9,10 +9,19 @@ class Persons extends Component {
 
 
   
-  shouldComponentUpdate(nextProps, newState){
-    console.log("[Persons.js] shouldComponentUpdate")
-    return true
-  }
+  // shouldComponentUpdate(nextProps, newState){
+  //   console.log("[Persons.js] shouldComponentUpdate")
+  //   if ( 
+  //     nextProps.persons !== this.props.persons || 
+  //     nextProps.changed !== this.props.changed || 
+  //     nextProps.clicked !== this.props.clicked
+  //   ) {
+  //     return true
+  //   } else {
+  //     return false;
+  //   }
+ 
+  // }
 
   getSnapshotBeforeUpdate(prevProps, prevState){
     console.log("[Persons.js] getSnapshotBeforeUpdate")
